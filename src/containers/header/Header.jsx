476c9d7e44bbf9cb6508caec000bcac5563cd08c
Mohamed Hussein <abdikafimohamed441@gmail.com>
2023-07-26 12:50:52 +0300

    test deploy

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,39 @@
+import { useEffect, useRef } from "react";
 import { Navbar } from "../../components";
 import "./header.css";
 
 const Header = () => {
+  const headerRef = useRef(null);
+
+  useEffect(() => {
+    const header = headerRef.current;
+    const observerOptions = {
+      rootMargin: "0px",
+      threshold: 1.0, // Intersection ratio of 1.0 means fully visible (sticky)
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // Header is sticky
+          header.classList.add("sticky");
+        } else {
+          // Header is not sticky
+          header.classList.remove("sticky");
+        }
+      });
+    }, observerOptions);
+
+    if (header) {
+      observer.observe(header);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
   return (
-    <div className="header">
+    <div className="header" ref={headerRef}>
       <Navbar />
     </div>
   );
